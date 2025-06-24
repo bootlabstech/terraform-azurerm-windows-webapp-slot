04e686faa09a9c6edba35b363559c13e6df80eda
Bhargava Naidu <bhargava@bootlabstech.com>
2025-06-24 13:20:35 +0530

    updated the variables and main config, added .gitignore

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "azurerm_windows_web_app_slot" "web_app_slot" {
   app_service_id = var.app_service_id
   virtual_network_subnet_id = var.virtual_network_subnet_id
 
+
   site_config {
     ftps_state       = var.ftps_state
     app_command_line = var.app_command_line
@@ -116,11 +117,11 @@ resource "azurerm_windows_web_app_slot" "web_app_slot" {
 
     dynamic "application_stack" {
       for_each = var.current_stack == "docker" ? [1] : []
-      content {
-        docker_container_name       = var.stack_version
-        # docker_registry_url      = var.docker_registry_url
-        # docker_registry_username = var.docker_registry_username
-        # docker_registry_password = var.docker_registry_password
+      content {        
+        docker_image_name       = var.stack_version
+        docker_registry_url      = var.docker_registry_url
+        docker_registry_username = var.docker_registry_username
+        docker_registry_password = var.docker_registry_password
       }
     }
     dynamic "application_stack" {
@@ -159,7 +160,7 @@ resource "azurerm_windows_web_app_slot" "web_app_slot" {
         python = var.python
       }
     }
-    virtual_application {
+  virtual_application {
     preload = true
     physical_path = var.physical_path
     virtual_path = var.virtual_path
--- variables.tf
+++ variables.tf
@@ -31,57 +31,57 @@ variable "stack_version" {
 
 }
 
-# variable "docker_registry_url" {
-#   type        = string
-#   description = "The URL of the container registry where the docker_image_name is located. e.g. https://index.docker.io or https://mcr.microsoft.com. This value is required with docker_image_name."
-#   default     = "https://mcr.microsoft.com"
-# }
-# variable "docker_registry_username" {
-#   type        = string
-#   description = "The User Name to use for authentication against the registry to pull the image."
-#   default     = "admin"
+variable "docker_registry_url" {
+  type        = string
+  description = "The URL of the container registry where the docker_image_name is located. e.g. https://index.docker.io or https://mcr.microsoft.com. This value is required with docker_image_name."
+  default     = "https://mcr.microsoft.com"
+}
+variable "docker_registry_username" {
+  type        = string
+  description = "The User Name to use for authentication against the registry to pull the image."
+  default     = "admin"
 
-# }
-# variable "docker_registry_password" {
-#   type        = string
-#   description = "The User Name to use for authentication against the registry to pull the image."
-#   default     = "Welcome@1234"
+}
+variable "docker_registry_password" {
+  type        = string
+  description = "The User Name to use for authentication against the registry to pull the image."
+  default     = "Welcome@1234"
 
-# }
+}
 
 variable "python" {
   type        = bool
-  description = "value"
+  description = "Enable Python runtime stack (true for Python-based apps, false otherwise)."
   default     = false
-
 }
+
 variable "virtual_network_subnet_id" {
   type        = string
-  description = "value"
-  
+  description = "The resource ID of the subnet to integrate the App Service with a virtual network."
 }
+
 variable "vnet_route_all_enabled" {
   type        = bool
-  description = "value"
-  
+  description = "Route all outbound App Service traffic through the integrated virtual network."
+  default = false
 }
+
 variable "physical_path" {
-    type =  string
-    description = "value"
-  
+  type        = string
+  description = "The physical file system path inside the App Service where the virtual directory maps."
 }
+
 variable "virtual_path" {
-    type = string
-    description = "value"
-  
+  type        = string
+  description = "The virtual URL path users use to access content served from 'physical_path'."
 }
+
 variable "physical_path1" {
-    type =  string
-    description = "value"
-  
+  type        = string
+  description = "An additional physical path for configuring a second virtual directory mapping."
 }
+
 variable "virtual_path1" {
-    type = string
-    description = "value"
-  
+  type        = string
+  description = "An additional virtual path for accessing 'physical_path1' in the web application."
 }
